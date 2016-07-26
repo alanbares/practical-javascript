4ea833903e7f93218df1a92d10c47556ea8ed871
alanbares <neloice@gmail.com>
2016-07-26 14:20:12 -0700

    If todolist is empty log to the console that the todo list is empty

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ var todoList = {
   todos: [],
   
   displayTodos: function() {
-    for(var i = 0; i<this.todos.length; i++) {
-      console.log(this.todos[i].todoText);
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty')
+    } else {
+			console.log('My Todos');
+			for(var i = 0; i<this.todos.length; i++) {
+				console.log(this.todos[i].todoText);
+    	}
     }
+      
   },
   
   addTodo: function(todoText) {
